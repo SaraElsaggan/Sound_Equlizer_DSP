862b3f6bff44e50688277e6b2254bb3d23f9afbe
SaraElsaggan <sara.mahmoud.sm600@gmail.com>
2023-12-19 11:02:03 +0200

    pc4 up animal is mess gaussien problem  and font not yet

--- app.py
+++ app.py
@@ -88,19 +88,23 @@ class MyWindow(QMainWindow):
         
        
         self.freq_ranges = {
+            "wolf" :  [(187.5, 1300)] ,
             "horse" :   [(1300, 3300)], 
             "bat" :  [(3300, 6000)], 
-            "wolf" :  [(170, 1290)] ,
-            "duck" :   [(1300, 7500)],
-            
+            # "wolf" :  [(170, 1290)] ,
+            # "duck" :   [(3000 , 7000)],
+            "duck" :   [(2000, 7500) , (16000, 20000)],
+
             "triangle" : [(4200, 22000)] ,
             "voil" : [(504 , 556 ) , (1014 , 1070) ,  (1530 , 1601) , (2048 , 2120) , (2566 , 2644) , (3080 , 3190) , (3600, 3710 ) , (4120 , 4220)], # new (done but replaced with noise)
-            "piano" : [(260 , 264 ) , (520 , 532) ,  (780 , 790) ,  (1045 , 1052) , (1574 , 1584) , (1840 , 1850)], #done
+            "piano" : [(0 ,10) , (60 , 80) , (100 , 200) , (240 , 280 ) , (260 , 264 ) , (480 , 580), (520 , 532) ,  (780 , 790) ,  (1045 , 1052) , (1574 , 1584) , (1840 , 1850) , (2000 , 2200) , (2350 , 2450) , (2640 , 2680) , (2900 , 2950 ), (3180,3260) ], #done
             "xylophone" : [(300, 1000) ],    
           
             "arthmya_1" : [( 0,12 )], 
-            "arthmya_2" : [(81 , 100) , (58 , 75) , (160, 175)], 
-            "arthmya_3" : [(95 , 105)  , (140 , 155) ], 
+            "arthmya_2" : [(405 , 589)], 
+            
+            "arthmya_3" : [(40 , 70) ], 
+            # "arthmya_3" : [(95 , 105)  , (140 , 155) ], 
 
         }
         for i in range(10):
@@ -110,25 +114,48 @@ class MyWindow(QMainWindow):
         for name in self.freq_ranges:
             slider = getattr(self.ui, f"{name}_slider")
             slider.valueChanged.connect(lambda value, n=name: self.modfy_frq_component(self.freq_ranges[n], value))
+      
+            # slider.valueChanged.connect(lambda value : self.update_tooltip(slider , value))
+            slider.valueChanged.connect(lambda value, s=slider: self.update_tooltip(s, value))
+
+            
+            # slider.setToolTip(f"{slider.value()}")
 
     
 
         QShortcut(QKeySequence("Ctrl+o"), self).activated.connect(self.upload_signal_file)
-    
         QShortcut(QKeySequence("Ctrl+m"), self).activated.connect(lambda :self.ui.combo_bx_mode.setCurrentIndex(1))
         QShortcut(QKeySequence("Ctrl+b"), self).activated.connect(lambda :self.ui.combo_bx_mode.setCurrentIndex(3))
         QShortcut(QKeySequence("Ctrl+n"), self).activated.connect(lambda :self.ui.combo_bx_mode.setCurrentIndex(2))
         QShortcut(QKeySequence("Ctrl+u"), self).activated.connect(lambda :self.ui.combo_bx_mode.setCurrentIndex(0))
 
+        QShortcut(QKeySequence("Ctrl+z"), self).activated.connect(self.save_sound_file)
+        QShortcut(QKeySequence("Ctrl+s"), self).activated.connect(self.save_ecg_file)
 
+    def save_sound_file(self):
+        modified_file_path, _ = QFileDialog.getSaveFileName(self, "Save Modified Signal", "~", "WAV Files (*.wav);;All Files (*)")
+        # output_file_path = "path/to/your/output/file.wav"
+        if modified_file_path:
+            wavfile.write(modified_file_path, self.sample_rate, self.modified_signal)
+        
+    def save_ecg_file(self):
+        # modified_df = pd.DataFrame({np.arange(0, len(self.modified_signal)) / self.sample_rate, self.modified_signal})
+        # modified_df = pd.DataFrame({np.arange(0, len(self.modified_signal)) / self.sample_rate, self.modified_signal})
+        # modified_df = pd.DataFrame([np.arange(0, len(self.modified_signal)) / self.sample_rate, self.modified_signal])
+# 
 
+        modified_df = pd.DataFrame({'Time': np.arange(0, len(self.modified_signal)) / self.sample_rate,'Modified_Signal': self.modified_signal})
+        
+        modified_file_path, _ = QFileDialog.getSaveFileName(self, "Save Modified Signal", "~", "CSV Files (*.csv)")
+        if modified_file_path:
+                modified_df.to_csv(modified_file_path, index=False)
+    
+    def update_tooltip(self , slider , slider_value):
+        # Set the tooltip to be the current value of the slider
+        slider.setToolTip(f"{str(slider_value)} db")
+        
     def handleComboBox(self, index):
         self.ui.stackedWidget.setCurrentIndex(index)
-    
-    def update_slider(self , slider):
-        current_value = slider.value()
-        if current_value < slider.maximum():
-            slider.setValue(current_value + 100)  
    
     def uniform_ranges(self ):
         freq_batches = np.array_split(self.frequency, 10)
@@ -146,19 +173,19 @@ class MyWindow(QMainWindow):
     
    
    
-        
+         
     def upload_signal_file(self):
         
-        file_path , _ = QFileDialog.getOpenFileName(self, "Open Song", "~")
+        self.file_path , _ = QFileDialog.getOpenFileName(self, "Open file", "~")
         
-        if file_path[-3:]== "csv":
-            df = pd.read_csv(file_path)
+        if self.file_path[-3:]== "csv":
+            df = pd.read_csv(self.file_path)
             self.time = df.iloc[:, 0].values
             self.original_sig = df.iloc[:, 1].values
             self.sample_rate = 1/(self.time[1]-self.time[0])
             
-        elif file_path[-3:]== "wav":
-            self.sample_rate, self.original_sig = wavfile.read(file_path)
+        elif self.file_path[-3:]== "wav":
+            self.sample_rate, self.original_sig = wavfile.read(self.file_path)
             self.time = np.array(range(0 , len(self.original_sig) )) / self.sample_rate
 
         self.plot_signal(self.time , self.original_sig , self.sample_rate , self.ui.grph_input_sig)
@@ -170,6 +197,7 @@ class MyWindow(QMainWindow):
         self.uniform_ranges()
         
     def plot_signal(self ,time ,  samples , sampling_rate , widget):
+    
 
         self.length = samples.shape[0] / sampling_rate
         time = np.array(range(0 , len(samples) )) / sampling_rate
@@ -201,6 +229,7 @@ class MyWindow(QMainWindow):
         elif window_type == 2:
             window = np.hanning(length)
         elif window_type == 3:
+            # window = gaussian(length , std = int(self.ui.std_input.text()))
             window = gaussian(length , std = int(self.ui.std_input.text()))
 
         
@@ -217,17 +246,29 @@ class MyWindow(QMainWindow):
         
         
     def modfy_frq_component(self, freq_range , slider_gain ):
+        
         all_indices = np.array([], dtype=int)
         for range in freq_range:
             indices_to_modify = np.where((self.frequency >= range[0]) & (self.frequency <= range[1]))[0]
+            #self.frequency >= range[0]
             all_indices = np.concatenate((all_indices, indices_to_modify))
         
         
 
         all_indices = np.sort(all_indices)
         window = self.window_function( len(self.magnitude[all_indices])  , self.ui.windows_tabs.currentIndex() ) 
+        # self.ui.xylophone_slider.setMinimum(-50)
+        # self.ui.xylophone_slider.setMaximum(50)
+        # # min_db = -50
+        # max_db = 50
+        # max_linear_value = 100
 
-        self.magnitude_to_bodfy[all_indices] = self.magnitude[all_indices] * slider_gain *window
+        # db_value = min_db + (slider_gain / max_linear_value) * (max_db - min_db)
+        self.magnitude_to_bodfy[all_indices] = self.magnitude[all_indices] * (10 ** (slider_gain / 20))*window
+        # print(f"slider_value :{self.ui.xylophone_slider.value()}")
+        # x = 10 ** (slider_gain / 20)
+        # print(f"db_value :{x}")
+        # self.magnitude_to_bodfy[all_indices] = self.magnitude[all_indices] * slider_gain *window
 
         
         
@@ -268,7 +309,7 @@ class MyWindow(QMainWindow):
     def reset_slider(self):
         for name in self.freq_ranges:
             slider = getattr(self.ui, f"{name}_slider")
-            slider.setValue(1)
+            slider.setValue(0)
 
 
     def pause(self , timer):
@@ -298,7 +339,12 @@ class MyWindow(QMainWindow):
         self.ui.grph_input_sig.getViewBox().scaleBy((state, state))
         self.ui.grph_output_sig.getViewBox().scaleBy((state, state))
         
-        
+     
+    def update_slider(self , slider):
+        current_value = slider.value()
+        if current_value < slider.maximum():
+            slider.setValue(current_value + 100)  
+   
       
 
 def main():
